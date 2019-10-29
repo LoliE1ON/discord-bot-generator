1bffd960e411b1e8728cb26359826271556d109c
sdf <trohimovkavad@gmail.com>
2019-10-29 15:20:17 +0300

    create base page, styles

---
+++ components/CreateBotTool.js
@@ -0,0 +1,34 @@
+module.exports = class CreateBotTool {
+
+    constructor (messagesContainer) {
+
+        this.messagesContainer = messagesContainer;
+        this.messages = {
+            ping: 'pong',
+            test: '123'
+        };
+
+        console.log("test");
+
+    }
+
+    addNewMessage() {
+
+        let messageBlock = `
+        <div id="message" class="uk-child-width-1-2 uk-text-center uk-margin" uk-grid>
+            <div>
+                <div class="uk-margin">
+                    <input class="uk-input" type="text" placeholder="Request">
+                </div>
+            </div>
+            <div>
+                <div class="uk-margin">
+                    <input class="uk-input" type="text" placeholder="Response">
+                </div>
+            </div>
+        </div>`;
+
+        this.messagesContainer.insertAdjacentHTML('afterend', messageBlock);
+    }
+
+}
